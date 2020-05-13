f29b6a03cd7bf4622745ddb651750a69cf3dfd76
Matthew Beauregard Smith <mbsmith93@utexas.edu>
2020-05-13 09:47:36 -0500

    more files for limiting failed edmans

---
+++ cc_code/src/common/approximation_model.cc
@@ -0,0 +1,9 @@
+// Author: Matthew Beauregard Smith (UT Austin)
+#include "approximation_model.h"
+
+namespace fluoroseq {
+
+ApproximationModel::ApproximationModel(int max_failed_edmans)
+        : max_failed_edmans(max_failed_edmans) {}
+
+}  // namespace fluoroseq
