b7eb48439eee435944d7e23077f3e9749fdc8371
Matthew Beauregard Smith <mbsmith93@utexas.edu>
2020-08-06 17:43:33 -0500

    improve readability of src/util/time.h

--- cc_code/src/util/time.h
+++ cc_code/src/util/time.h
@@ -4,33 +4,26 @@
 #ifndef FLUOROSEQ_UTIL_TIME_H
 #define FLUOROSEQ_UTIL_TIME_H
 
+// Technically including mpi.h again for the "mpi.h has been included" case is
+// redundant, since MPI_VERSION is included in mpi.h, so it must already be
+// present, however this may make the file easier to handle in some code
+// editors.
 #ifdef MPI_VERSION  // if mpi.h has been included.
-
-// Technically including mpi.h is redundant, since MPI_VERSION is included in
-// mpi.h, so it must already be present, however this may make the file easier
-// to handle in some code editors.
 #include <mpi.h>
-
-namespace fluoroseq {
-
-double wall_time() {
-    return MPI_Wtime();
-}
-
-}  // namespace fluoroseq
-
 #else  // if mpi.h has not been included
-
 #include <ctime>
+#endif  // MPI_VERSION
 
 namespace fluoroseq {
 
 double wall_time() {
+#ifdef MPI_VERSION  // if mpi.h has been included.
+    return MPI_Wtime();
+#else  // if mpi.h has not been included.
     return (double) clock() / (double) CLOCKS_PER_SEC;
+#endif  // MPI_VERSION
 }
 
 }  // namespace fluoroseq
-
-#endif  // MPI_VERSION
 
 #endif  // FLUOROSEQ_UTIL_TIME_H
