e980be18d329147236619809068229ef43314358
Matthew Beauregard Smith <mbsmith93@utexas.edu>
2020-08-06 11:05:35 -0500

    modified util/time.h to work for both MPI and non-MPI code in a more coherent way.

--- cc_code/src/util/time.h
+++ cc_code/src/util/time.h
@@ -1,14 +1,36 @@
 // Author: Matthew Beauregard Smith (UT Austin)
 //
-// This file corresponds to two possible .cc files. If not using MPI, build with
-// time.cc. If using MPI, build with mpi_time.cc.
+// IF USING MPI, YOU MUST INCLUDE <mpi.h> BEFORE INCLUDING THIS FILE.
 #ifndef FLUOROSEQ_UTIL_TIME_H
 #define FLUOROSEQ_UTIL_TIME_H
 
+#ifdef MPI_VERSION  // if mpi.h has been included.
+
+// Technically including mpi.h is redundant, since MPI_VERSION is included in
+// mpi.h, so it must already be present, however this may make the file easier
+// to handle in some code editors.
+#include <mpi.h>
+
 namespace fluoroseq {
 
-double wall_time();
+double wall_time() {
+    return MPI_Wtime();
+}
 
 }  // namespace fluoroseq
+
+#else  // if mpi.h has not been included
+
+#include <ctime>
+
+namespace fluoroseq {
+
+double wall_time() {
+    return (double) clock() / (double) CLOCKS_PER_SEC;
+}
+
+}  // namespace fluoroseq
+
+#endif  // MPI_VERSION
 
 #endif  // FLUOROSEQ_UTIL_TIME_H
