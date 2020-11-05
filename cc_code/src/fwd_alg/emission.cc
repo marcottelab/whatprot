318c68d578f10cc40790650fbd9ce64a3b5a7a85
Matthew Beauregard Smith <mbsmith93@utexas.edu>
2020-11-04 19:25:59 -0600

    fixing minor bug in emission calculation code

--- cc_code/src/fwd_alg/emission.cc
+++ cc_code/src/fwd_alg/emission.cc
@@ -19,10 +19,10 @@ Emission::Emission(const Radiometry& radiometry,
         : num_timesteps(radiometry.num_timesteps),
           num_channels(radiometry.num_channels),
           max_num_dyes(max_num_dyes) {
-    values.resize(num_timesteps * num_channels * max_num_dyes);
+    values.resize(num_timesteps * num_channels * (max_num_dyes + 1));
     for (int t = 0; t < num_timesteps; t++) {
         for (int c = 0; c < num_channels; c++) {
-            for (int d = 0; d < max_num_dyes; d++) {
+            for (int d = 0; d < (max_num_dyes + 1); d++) {
                 prob(t, c, d) = pdf(radiometry(t, c), d);
             }
         }
@@ -30,11 +30,11 @@ Emission::Emission(const Radiometry& radiometry,
 }
 
 double& Emission::prob(int t, int c, int d) {
-    return values[(t * num_channels + c) * max_num_dyes + d];
+    return values[(t * num_channels + c) * (max_num_dyes + 1) + d];
 }
 
 double Emission::prob(int t, int c, int d) const {
-    return values[(t * num_channels + c) * max_num_dyes + d];
+    return values[(t * num_channels + c) * (max_num_dyes + 1) + d];
 }
 
 void Emission::operator()(Tensor* tensor, int timestep) const {
