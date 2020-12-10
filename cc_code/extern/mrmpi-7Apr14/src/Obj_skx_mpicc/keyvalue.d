d1fc1967ceba3ac234c1e2a8877fa8fe09a2fb3b
Matthew Beauregard Smith <mbsmith93@utexas.edu>
2020-12-10 11:01:28 -0600

    fixing mrmpi to work right for TACC SKX systems

--- cc_code/extern/mrmpi-7Apr14/src/Obj_skx_mpicc/keyvalue.d
+++ cc_code/extern/mrmpi-7Apr14/src/Obj_skx_mpicc/keyvalue.d
@@ -1,29 +1,31 @@
 keyvalue.o: keyvalue.cpp /usr/include/stdc-predef.h \
- /opt/intel/compilers_and_libraries_2018.2.199/linux/mpi/intel64/include/mpi.h \
- /opt/intel/compilers_and_libraries_2018.2.199/linux/compiler/include/stdint.h \
- /opt/apps/gcc/6.3.0/lib/gcc/x86_64-pc-linux-gnu/6.3.0/include/stdint.h \
+ /opt/intel/compilers_and_libraries_2017.4.196/linux/mpi/intel64/include/mpi.h \
+ /opt/apps/gcc/7.1.0/lib/gcc/x86_64-pc-linux-gnu/7.1.0/include/stdint.h \
  /usr/include/stdint.h /usr/include/features.h /usr/include/sys/cdefs.h \
  /usr/include/bits/wordsize.h /usr/include/gnu/stubs.h \
  /usr/include/gnu/stubs-64.h /usr/include/bits/wchar.h \
- /opt/intel/compilers_and_libraries_2018.2.199/linux/mpi/intel64/include/mpio.h \
- /opt/intel/compilers_and_libraries_2018.2.199/linux/mpi/intel64/include/mpicxx.h \
+ /opt/intel/compilers_and_libraries_2017.4.196/linux/mpi/intel64/include/mpio.h \
+ /opt/intel/compilers_and_libraries_2017.4.196/linux/mpi/intel64/include/mpi.h \
+ /opt/intel/compilers_and_libraries_2017.4.196/linux/mpi/intel64/include/mpicxx.h \
  /usr/include/stdio.h \
- /opt/intel/compilers_and_libraries_2018.2.199/linux/compiler/include/stddef.h \
- /usr/include/bits/types.h /usr/include/bits/typesizes.h /usr/include/libio.h \
- /usr/include/_G_config.h /usr/include/wchar.h \
- /opt/intel/compilers_and_libraries_2018.2.199/linux/compiler/include/stdarg.h \
+ /opt/apps/gcc/7.1.0/lib/gcc/x86_64-pc-linux-gnu/7.1.0/include/stddef.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/libio.h /usr/include/_G_config.h /usr/include/wchar.h \
+ /opt/apps/gcc/7.1.0/lib/gcc/x86_64-pc-linux-gnu/7.1.0/include/stdarg.h \
  /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
- /usr/include/bits/stdio.h /opt/apps/gcc/6.3.0/include/c++/6.3.0/stdlib.h \
- /opt/apps/gcc/6.3.0/include/c++/6.3.0/cstdlib \
- /opt/apps/gcc/6.3.0/include/c++/6.3.0/x86_64-pc-linux-gnu/bits/c++config.h \
- /opt/apps/gcc/6.3.0/include/c++/6.3.0/x86_64-pc-linux-gnu/bits/os_defines.h \
- /opt/apps/gcc/6.3.0/include/c++/6.3.0/x86_64-pc-linux-gnu/bits/cpu_defines.h \
+ /usr/include/bits/stdio.h /opt/apps/gcc/7.1.0/include/c++/7.1.0/stdlib.h \
+ /opt/apps/gcc/7.1.0/include/c++/7.1.0/cstdlib \
+ /opt/apps/gcc/7.1.0/include/c++/7.1.0/x86_64-pc-linux-gnu/bits/c++config.h \
+ /opt/apps/gcc/7.1.0/include/c++/7.1.0/x86_64-pc-linux-gnu/bits/os_defines.h \
+ /opt/apps/gcc/7.1.0/include/c++/7.1.0/x86_64-pc-linux-gnu/bits/cpu_defines.h \
  /usr/include/stdlib.h /usr/include/bits/waitflags.h \
- /usr/include/bits/waitstatus.h /usr/include/endian.h /usr/include/bits/endian.h \
- /usr/include/bits/byteswap.h /usr/include/bits/byteswap-16.h \
- /usr/include/xlocale.h /usr/include/sys/types.h /usr/include/time.h \
- /usr/include/sys/select.h /usr/include/bits/select.h /usr/include/bits/sigset.h \
+ /usr/include/bits/waitstatus.h /usr/include/endian.h \
+ /usr/include/bits/endian.h /usr/include/bits/byteswap.h \
+ /usr/include/bits/byteswap-16.h /usr/include/xlocale.h \
+ /usr/include/sys/types.h /usr/include/time.h /usr/include/sys/select.h \
+ /usr/include/bits/select.h /usr/include/bits/sigset.h \
  /usr/include/bits/time.h /usr/include/sys/sysmacros.h \
  /usr/include/bits/pthreadtypes.h /usr/include/alloca.h \
- /usr/include/bits/stdlib-float.h /usr/include/string.h keyvalue.h mapreduce.h \
- memory.h error.h
+ /usr/include/bits/stdlib-float.h \
+ /opt/apps/gcc/7.1.0/include/c++/7.1.0/bits/std_abs.h \
+ /usr/include/string.h keyvalue.h mapreduce.h memory.h error.h
--- cc_code/extern/mrmpi-7Apr14/src/Obj_skx_serial/mapreduce.d
+++ cc_code/extern/mrmpi-7Apr14/src/Obj_skx_serial/mapreduce.d
@@ -1,30 +1,32 @@
-mapreduce.o: mapreduce.cpp /usr/include/stdc-predef.h ../../mpistubs/mpi.h \
- /usr/include/ctype.h /usr/include/features.h /usr/include/sys/cdefs.h \
- /usr/include/bits/wordsize.h /usr/include/gnu/stubs.h \
- /usr/include/gnu/stubs-64.h /usr/include/bits/types.h \
- /usr/include/bits/typesizes.h /usr/include/endian.h /usr/include/bits/endian.h \
+mapreduce.o: mapreduce.cpp /usr/include/stdc-predef.h \
+ ../../mpistubs/mpi.h /usr/include/ctype.h /usr/include/features.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/wordsize.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/endian.h /usr/include/bits/endian.h \
  /usr/include/bits/byteswap.h /usr/include/bits/byteswap-16.h \
  /usr/include/xlocale.h /usr/include/string.h \
- /opt/intel/compilers_and_libraries_2018.2.199/linux/compiler/include/stddef.h \
+ /opt/apps/gcc/7.1.0/lib/gcc/x86_64-pc-linux-gnu/7.1.0/include/stddef.h \
  /usr/include/stdio.h /usr/include/libio.h /usr/include/_G_config.h \
  /usr/include/wchar.h \
- /opt/intel/compilers_and_libraries_2018.2.199/linux/compiler/include/stdarg.h \
+ /opt/apps/gcc/7.1.0/lib/gcc/x86_64-pc-linux-gnu/7.1.0/include/stdarg.h \
  /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
- /usr/include/bits/stdio.h /opt/apps/gcc/6.3.0/include/c++/6.3.0/stdlib.h \
- /opt/apps/gcc/6.3.0/include/c++/6.3.0/cstdlib \
- /opt/apps/gcc/6.3.0/include/c++/6.3.0/x86_64-pc-linux-gnu/bits/c++config.h \
- /opt/apps/gcc/6.3.0/include/c++/6.3.0/x86_64-pc-linux-gnu/bits/os_defines.h \
- /opt/apps/gcc/6.3.0/include/c++/6.3.0/x86_64-pc-linux-gnu/bits/cpu_defines.h \
+ /usr/include/bits/stdio.h /opt/apps/gcc/7.1.0/include/c++/7.1.0/stdlib.h \
+ /opt/apps/gcc/7.1.0/include/c++/7.1.0/cstdlib \
+ /opt/apps/gcc/7.1.0/include/c++/7.1.0/x86_64-pc-linux-gnu/bits/c++config.h \
+ /opt/apps/gcc/7.1.0/include/c++/7.1.0/x86_64-pc-linux-gnu/bits/os_defines.h \
+ /opt/apps/gcc/7.1.0/include/c++/7.1.0/x86_64-pc-linux-gnu/bits/cpu_defines.h \
  /usr/include/stdlib.h /usr/include/bits/waitflags.h \
- /usr/include/bits/waitstatus.h /usr/include/sys/types.h /usr/include/time.h \
- /usr/include/sys/select.h /usr/include/bits/select.h /usr/include/bits/sigset.h \
- /usr/include/bits/time.h /usr/include/sys/sysmacros.h \
- /usr/include/bits/pthreadtypes.h /usr/include/alloca.h \
- /usr/include/bits/stdlib-float.h \
- /opt/intel/compilers_and_libraries_2018.2.199/linux/compiler/include/stdint.h \
- /opt/apps/gcc/6.3.0/lib/gcc/x86_64-pc-linux-gnu/6.3.0/include/stdint.h \
+ /usr/include/bits/waitstatus.h /usr/include/sys/types.h \
+ /usr/include/time.h /usr/include/sys/select.h /usr/include/bits/select.h \
+ /usr/include/bits/sigset.h /usr/include/bits/time.h \
+ /usr/include/sys/sysmacros.h /usr/include/bits/pthreadtypes.h \
+ /usr/include/alloca.h /usr/include/bits/stdlib-float.h \
+ /opt/apps/gcc/7.1.0/include/c++/7.1.0/bits/std_abs.h \
+ /opt/apps/gcc/7.1.0/lib/gcc/x86_64-pc-linux-gnu/7.1.0/include/stdint.h \
  /usr/include/stdint.h /usr/include/bits/wchar.h /usr/include/sys/stat.h \
- /usr/include/bits/stat.h /usr/include/dirent.h /usr/include/bits/dirent.h \
- /usr/include/bits/posix1_lim.h /usr/include/bits/local_lim.h \
- /usr/include/linux/limits.h mapreduce.h version.h mrtype.h keyvalue.h \
- keymultivalue.h spool.h irregular.h hash.h memory.h error.h
+ /usr/include/bits/stat.h /usr/include/dirent.h \
+ /usr/include/bits/dirent.h /usr/include/bits/posix1_lim.h \
+ /usr/include/bits/local_lim.h /usr/include/linux/limits.h mapreduce.h \
+ version.h mrtype.h keyvalue.h keymultivalue.h spool.h irregular.h hash.h \
+ memory.h error.h
