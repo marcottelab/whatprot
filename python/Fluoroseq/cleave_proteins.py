d397244cde6fdb72eebb3097936255205fa76542
mbsmith93 <mbsmith93@gmail.com>
2022-04-22 15:41:06 -0500

    Refactoring cleave_proteins.py to make future changes easier; avoiding redundancies.

--- python/Fluoroseq/cleave_proteins.py
+++ python/Fluoroseq/cleave_proteins.py
@@ -31,41 +31,32 @@ def cleave_proteins(fasta, fpeptides, protease, n = -1):
     print(len(npros))
     fpep = open(fpeptides, "w")
     # Conventionally fasta files have proteins from N-terminus to C-terminus
-    if (protease == "trypsin"):
-        # Now we trypsinize, cutting the C-terminal side of lysine (K) and
-        # arginine (R) residues, but only if they are not followed by Proline
-        # (P).
-        for protein in npros:
-            lastcut = 0
-            for i in range(len(protein) - 1):
+    for protein in npros:
+        lastcut = 0
+        for i in range(len(protein) - 1):
+            cut = False
+            if (protease == "trypsin"):
+                # Here we trypsinize, cutting the C-terminal side of lysine (K)
+                # and arginine (R) residues, but only if they are not followed
+                # by Proline (P).
                 if ((protein[i] == 'K' or protein[i] == 'R') and (protein[i + 1] != 'P')):
-                    peptide = protein[lastcut : i + 1]
-                    lastcut = i + 1
-                    fpep.write(peptide + "\n")
-            peptide = protein[lastcut:]
-            fpep.write(peptide + "\n")
-    elif (protease == "cyanogen bromide"):
-        # Now we use cyanogen bromide, cutting the C-terminal side of methionine
-        # (M).
-        for protein in npros:
-            lastcut = 0
-            for i in range(len(protein) - 1):
+                    cut = True
+            elif (protease == "cyanogen bromide"):
+                # Now we use cyanogen bromide, cutting the C-terminal side of
+                # methionine (M).
                 if ((protein[i] == 'M')):
-                    peptide = protein[lastcut : i + 1]
-                    lastcut = i + 1
-                    fpep.write(peptide + "\n")
-            peptide = protein[lastcut:]
-            fpep.write(peptide + "\n")
-    elif (protease == "EndoPRO"):
-        # Here we use EndoPRO, cutting the C-terminal side of proline (P) and
-        # alanine (A).
-        for protein in npros:
-            lastcut = 0
-            for i in range(len(protein) - 1):
+                    cut = True
+            elif (protease == "EndoPRO"):
+                # Here we use EndoPRO, cutting the C-terminal side of proline
+                # (P) and alanine (A).
                 if ((protein[i] == 'P' or protein[i] == 'A')):
-                    peptide = protein[lastcut : i + 1]
-                    lastcut = i + 1
-                    fpep.write(peptide + "\n")
-            peptide = protein[lastcut:]
-            fpep.write(peptide + "\n")
+                    cut = True
+            else:
+                print('error, invalid protease: ' + protease)
+            if (cut == True):
+                peptide = protein[lastcut : i + 1]
+                lastcut = i + 1
+                fpep.write(peptide + "\n")
+        peptide = protein[lastcut:]
+        fpep.write(peptide + "\n")
     fpep.close()
