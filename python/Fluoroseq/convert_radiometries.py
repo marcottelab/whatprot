7f282f300b7dc28b89898810b76d448d7338c03d
mbsmith93 <mbsmith93@gmail.com>
2022-08-25 17:07:28 -0500

    fixing issue in radiometry conversion python script.

--- python/Fluoroseq/convert_radiometries.py
+++ python/Fluoroseq/convert_radiometries.py
@@ -6,11 +6,12 @@
 from numpy import load
 from numpy import transpose
 
-NUM_CHANNELS = 2
-NUM_CYCLES = 14
+NUM_CHANNELS = 1
+NUM_MOCKS = 0
+NUM_CYCLES = 4
 BETA = 15000
-RADMAT_FILE = 'C:/Users/Matthew/OneDrive/OdenInstitute/MarcotteLab/data/classification/esn_mx074_tau441/full_signal_radmat.npy'
-OUTPUT_FILE = 'C:/Users/Matthew/OneDrive/OdenInstitute/MarcotteLab/data/classification/esn_mx074_tau441/radiometries.tsv'
+RADMAT_FILE = 'C:/Users/mbsmi/OneDrive/OdenInstitute/MarcotteLab/data/classification/jim_jhm2022_07_04a_01_jsp127_1p2m10e_640_startcycle2/full_signal_radmat.npy'
+OUTPUT_FILE = 'C:/Users/mbsmi/OneDrive/OdenInstitute/MarcotteLab/data/classification/jim_jhm2022_07_04a_01_jsp127_1p2m10e_640_startcycle2/radiometries.tsv'
 
 radmat = load(RADMAT_FILE)
 
@@ -24,7 +25,7 @@ f.write(str(NUM_CYCLES) + "\n")
 f.write(str(NUM_CHANNELS) + "\n")
 f.write(str(radmat.shape[0]) + "\n")
 for rad in radmat:
-    for i in range(NUM_CYCLES):
+    for i in range(NUM_MOCKS, NUM_MOCKS + NUM_CYCLES):
         for j in range(NUM_CHANNELS):
             if i > 0 or j > 0:
                 f.write("\t")
