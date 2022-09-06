8bc84abb5432aead294ffcb612947c19fcb60a57
mbsmith93 <mbsmith93@gmail.com>
2022-09-06 16:38:10 -0500

    Making meta_all_to_results.py into a function instead of a script.

--- python/Fluoroseq/meta_all_to_results.py
+++ python/Fluoroseq/meta_all_to_results.py
@@ -5,24 +5,24 @@
 
 import csv
 
-META_ALL='C:/Users/mbsmi/OneDrive/OdenInstitute/MarcotteLab/data/classification/real-data-4peps-v-50/meta_all.tsv'
-RESULTS='C:/Users/mbsmi/OneDrive/OdenInstitute/MarcotteLab/data/classification/real-data-4peps-v-50/rf-predictions.csv'
+# Converts classification predictions from Erisyon into the .csv format given
+# by classification with whatprot.
+def meta_all_to_results(meta_all, results):
+    f_meta = open(meta_all, 'r')
+    meta_all = csv.reader(f_meta, delimiter='\t')
 
-f_meta = open(META_ALL, 'r')
-meta_all = csv.reader(f_meta, delimiter='\t')
+    f_results = open(results, 'w')
+    f_results.write('radmat_iz,best_pep_iz,best_pep_score\n')
 
-f_results = open(RESULTS, 'w')
-f_results.write('radmat_iz,best_pep_iz,best_pep_score\n')
+    idx = 0
+    for meta_row in meta_all:
+        f_results.write(str(idx))
+        f_results.write(',')
+        f_results.write(meta_row[6])
+        f_results.write(',')
+        f_results.write(meta_row[2])
+        f_results.write('\n')
+        idx += 1    
 
-idx = 0
-for meta_row in meta_all:
-    f_results.write(str(idx))
-    f_results.write(',')
-    f_results.write(meta_row[6])
-    f_results.write(',')
-    f_results.write(meta_row[2])
-    f_results.write('\n')
-    idx += 1    
-
-f_meta.close()
-f_results.close()
+    f_meta.close()
+    f_results.close()
