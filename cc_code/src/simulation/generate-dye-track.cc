8f0876d46be346f135f97ccfe10c1f675e1ea183
Matthew Beauregard Smith <mbsmith93@gmail.com>
2022-11-29 10:17:02 -0600

    Adding n-terminal blocking behavior to simulation code.

--- cc_code/src/simulation/generate-dye-track.cc
+++ cc_code/src/simulation/generate-dye-track.cc
@@ -34,6 +34,8 @@ void generate_dye_track(const SequencingModel& seq_model,
                         DyeTrack* dye_track) {
     bernoulli_distribution edman_failure(seq_model.p_edman_failure);
     bernoulli_distribution detach_event(seq_model.p_detach);
+    bernoulli_distribution initial_blocked_n(seq_model.p_initial_break_n);
+    bernoulli_distribution cyclic_blocked_n(seq_model.p_cyclic_break_n);
     vector<bernoulli_distribution> dud_events;
     vector<bernoulli_distribution> bleach_events;
     for (unsigned int c = 0; c < num_channels; c++) {
@@ -58,10 +60,18 @@ void generate_dye_track(const SequencingModel& seq_model,
     }
     unsigned int e = 0;  // Successful Edman cycles.
     unsigned int t = 0;  // Timesteps.
+    bool blocked_n = false;
+    if (initial_blocked_n(*generator)) {
+        blocked_n = true;
+    }
     while (true) {
         // Record results.
         for (unsigned int c = 0; c < num_channels; c++) {
             (*dye_track)(t, c) = counts[c];
+        }
+        // block n
+        if (cyclic_blocked_n(*generator)) {
+            blocked_n = true;
         }
         // Detach.
         if (detach_event(*generator)) {
@@ -72,14 +82,16 @@ void generate_dye_track(const SequencingModel& seq_model,
             break;
         }
         // Edman failures.
-        if (!edman_failure(*generator)) {
-            if (ds[e] != -1) {
-                counts[ds[e]]--;
+        if (!blocked_n) {
+            if (!edman_failure(*generator)) {
+                if (ds[e] != -1) {
+                    counts[ds[e]]--;
+                }
+                e++;
             }
-            e++;
-        }
-        if (e >= ds.length) {
-            break;
+            if (e >= ds.length) {
+                break;
+            }
         }
         // Bleaching.
         for (unsigned int i = e; i < ds.length; i++) {
