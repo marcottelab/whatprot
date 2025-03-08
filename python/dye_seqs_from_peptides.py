57c003694d3fcc897849eaea7ef45aeb5be3cde8
Matthew Beauregard Smith <mbsmith93@gmail.com>
2025-03-08 15:24:33 -0500

    not sure what this is but looks important...

--- python/dye_seqs_from_peptides.py
+++ python/dye_seqs_from_peptides.py
@@ -6,17 +6,22 @@
 from common.peptide import Peptide
 from simulate.label_peptides import label_peptides
 
-def dye_seqs_from_peptides(peptide_file, label_set, dye_seqs_file, mode='first'):
+def dye_seqs_from_peptides(peptide_file, label_set, dye_seqs_file, peps_prots_file, amts = None, mode='first'):
     f = open(peptide_file, 'r')
     # f.readline()  # header
     # f.readline()  # Zack's null line
     line = "placeholder"
     peptides = []
+    proteins = set()
     pep_id = 0
+    pep2prot = {}
     while line != '\n' and line != '':
         line = f.readline()[0 : -1]
-        line = line.split(",")[0]
-        peptides += [Peptide(line, pep_id=pep_id)]
+        pattern = line.split(",")[0]
+        peptides += [Peptide(pattern, pep_id=pep_id)]
+        prot_id = int(line.split(",")[1])
+        proteins.add(prot_id)
+        pep2prot[pep_id] = prot_id
         pep_id += 1
     f.close()
     dye_seqs = label_peptides(peptides, label_set)
@@ -35,4 +40,12 @@ def dye_seqs_from_peptides(peptide_file, label_set, dye_seqs_file, mode='first')
         else:
             print("bad-input, invalid mode: " + mode)
     f.close()
+
+    f = open(peps_prots_file, 'w')
+    proteins = list(proteins).sort()
+    f.write('protein,' + ','.join(proteins) + '\n')
+    f.write('amt,' + ','.join(amts) + '\n')
+    f.write('peptide\n')
+    for dye_seq in dye_seqs:
+        f.write()
 
